a71cc53587d6ac02b3e8828fcfd721e054940150
ADS-B related code <34561158+adsb-related-code@users.noreply.github.com>
2025-03-12 11:28:13 -0700

    Update fr24-enable.sh
    comment updates to prompt user for FR24 sharing key

--- helpers/fr24-enable.sh
+++ helpers/fr24-enable.sh
@@ -4,6 +4,10 @@
 # fix is relatively simple .. edit fr24_signup_expect to match current FR24 setup prompts
 # underlying problem if fr24feed --signup and --reconfigure failing with SSL error is another issue
 # consider making user sign up at FR24 and REQUIRE sharing key if FR24 code is broken in the future
+#
+# If you don't remember your sharing key, you can find it in your account on the website under "My data sharing".
+# https://www.flightradar24.com/account/data-sharing
+#
 
 source /boot/airplanes-config.txt
 FR24_EMAIL="$1"
