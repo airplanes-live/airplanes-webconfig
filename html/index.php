001645eb498edb964510060973cbc0cd8fe8aeb2
ADS-B related code <34561158+adsb-related-code@users.noreply.github.com>
2025-05-16 18:21:25 -0700

    Update index.php
    add webui version to start page

--- html/index.php
+++ html/index.php
@@ -55,7 +55,7 @@
 			<img src="img/airplaneslive.png" style="max-width: 600px;height: auto;" width="100%"/>
 			<br><br>
 			<h6>airplanes.live Feeder Image <br>V: <?php echo file_get_contents("/boot/airplanes-version"); ?></h6>
-			<h6>Bookworm Shenanigans Edition</h6>
+			<h6>Bookworm <br> V: <?php echo file_get_contents("/boot/airplanes-version-webconfig"); ?></h6>
 			<?php
         			$receivername = file_get_contents("/tmp/webconfig/name");
         			$location = file_get_contents("/tmp/webconfig/location");
